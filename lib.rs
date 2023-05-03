a6e059b2e3fadc5bcf1f8a0f908e15d9eb61845f
Adebara Ayomide <adebaraayomide@gmail.com>
2023-05-03 17:05:54 +0100

    Ayomide: Structures implemented

--- lib.rs
+++ lib.rs
@@ -1,12 +1,34 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 #[ink::contract]
-mod insure {
+pub mod insure {
 
+    // =====================================
+    // MAKING IMPORTATIONS 
+    // =====================================
+    use openbrush::{
+        contracts::{
+            traits::psp22::PSP22Ref,
+        },
+    }; // this would be used for psp22 token interaction 
     use ink::prelude::vec;
     use ink::storage::Mapping;
+    use ink::env::CallFlags;
+
     #[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
     #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+
+
+    pub enum Error {
+        NotAdmin,
+        AmountShouldBeGreaterThanZero,
+        InsufficientFunds,
+        NotEnoughAllowance,
+        TokenTransferFailed,
+        Overflow
+    }
+
+
     pub enum RiskLevel {
         VeryLow,
         Low,
@@ -14,6 +36,48 @@ mod insure {
         High,
         VeryHigh,
     }
+
+    // ===================================
+    // EVENTS DECLARATION 
+    // ===================================
+
+    #[ink(event)]
+    pub struct NewInsure {
+        #[ink(topic)]
+        protocol_name: String,
+        protocol_domain: String,
+        total_cover_created: Balance,
+        creator_address: AccountId,
+        risk_level: RiskLevel,
+        creation_time: Balance,
+        protocol_id: u64,
+    }
+
+    #[ink(event)] 
+    pub struct AddOnExistingInsure {
+        #[ink(topic)]
+        protocol_name: String,
+        protocol_domain: String,
+        cover_added: Balance,
+        creator_address: AccountId,
+        creation_time: Balance,
+    }
+
+    #[ink(event)] 
+    pub struct CoverBought {
+        #[ink(topic)]
+        protocol: String,
+        total_cover_bought: Balance,
+        amount_paid: Balance,
+        total_period: Balance,
+        risk_level: RiskLevel,
+    }
+
+
+    // ===================================
+    // STRUCTURES 
+    // ===================================
+
 
     pub struct RiskAssessor {
         total_cover_provided: Balance,
@@ -78,16 +142,123 @@ mod insure {
 
     }
 
+    #[ink(impl)]
+    impl Insurance {
+         // ================================================
+        // MODIFIERS, AUTHORIZATION GATE AND SANITY CHECKS 
+        // ================================================
+        
+        fn only_owner(&self) -> Result<(), Error> {
+            if self.env().caller() == self.admin {
+                Ok(())
+            } else {
+                Err(Error::NotAdmin)
+            }
+        }
 
-    #[ink(event)]
-    pub struct NewInsure {
-        #[ink(topic)]
-        protocol_name: String,
-        protocol_domain: String,
-        total_cover_created: Balance,
+        fn zero_address(&self) -> AccountId {
+            [0u8; 32].into()
+        }
+
+        fn transfer(
+            &self,
+            to: AccountId,
+            token: AccountId,
+            amount: Balance
+        ) -> Result<(), Error> { 
+            PSP22Ref::transfer(
+                &token,
+                to,
+                amount,
+                vec![])
+                    .unwrap_or_else(|error| {
+                    panic!(
+                        "Failed to transfer PSP22 2 tokens to caller : {:?}",
+                        error
+                    )
+            });
+
+            Ok(())
+        }
+
+
+        fn transfer_from(
+            &self,
+            from: AccountId,
+            to: AccountId,
+            token: AccountId,
+            amount: Balance
+        ) -> Result<(), Error> { 
+            // checking the balance of the sender to see if the sender has enough balance to run this transfer 
+            let user_current_balance = PSP22Ref::balance_of(
+                &token,
+                from
+            );
+
+            if user_current_balance < amount {
+                return Err(Error::InsufficientFunds)
+            }
+
+            // checking if enough allowance has been made for this operation 
+            let staking_contract_allowance = PSP22Ref::allowance(
+                &token,
+                from,
+                to
+            );
+
+            if staking_contract_allowance < amount {
+                return Err(Error::NotEnoughAllowance)
+            }
+
+            let staking_contract_initial_balance = PSP22Ref::balance_of(
+                &token,
+                to
+            );
+
+
+            // making the transfer call to the token contract 
+            if PSP22Ref::transfer_from_builder(
+                &token,
+                from,
+                to,
+                amount,
+                vec![])
+                    .call_flags(CallFlags::default()
+                    .set_allow_reentry(true))
+                    .try_invoke()
+                    .expect("Transfer failed")
+                    .is_err(){
+                        return Err(Error::TokenTransferFailed);
+            }
+
+            let staking_contract_balance_after_transfer = PSP22Ref::balance_of(
+                &token,
+                to
+            );
+
+            let mut actual_token_staked:Balance = 0;
+        
+            // calculating the actual amount that came in to the contract, some token might have taxes, just confirming transfer for economic safety
+            match staking_contract_balance_after_transfer.checked_sub(staking_contract_initial_balance) {
+                Some(result) => {
+                    actual_token_staked = result;
+                }
+                None => {
+                    return Err(Error::Overflow);
+                }
+            };
+
+            Ok(())
+        }
     }
 
+
+    #[ink(impl)]
     impl Insurance {
+
+        // ====================================
+        // MAIN IMPLEMENTATION BLOCK 
+        // ====================================
         /// Constructor that initializes the `bool` value to the given `init_value`.
         #[ink(constructor)]
         pub fn new(token_address: AccountId, governance_address: AccountId) -> Self {
@@ -118,21 +289,27 @@ mod insure {
             Self::new(Default::default())
         }
 
-        /// A message that can be called on instantiated contracts.
-        /// This one flips the value of the stored `bool` from `true`
-        /// to `false` and vice versa.
+    // ***************** //
+    
+    // WRITE FUNCTIONS
+    
+    // ***************** //
+
+        
         #[ink(message)]
-        pub fn create_new_insurance(&mut self) {
+        pub fn create_new_insurance(
+            &mut self,
+            _protocol_name: &str,
+            _protocol_domain: &str,
+            _description: &str,
+            total_cover_amount: Balance
+            ) -> Result<(), Error>
+        {
             
+            }
         }
 
-        /// Simply retu/// Constructor that initializes the `bool` value to `false`.
-        // ///
-        // /// Constructors can delegate to other constructors.
-        // #[ink(constructor)]
-        // pub fn default() -> Self {
-        //     Self::new(Default::default())
-        // }rns the current value of our `bool`.
+
         #[ink(message)]
         pub fn createOnExistinginsure(&mut self) {
           
@@ -140,75 +317,4 @@ mod insure {
     }
 
 
-    /// This is how you'd write end-to-end (E2E) or integration tests for ink! contracts.
-    ///
-    /// When running these you need to make sure that you:
-    /// - Compile the tests with the `e2e-tests` feature flag enabled (`--features e2e-tests`)
-    /// - Are running a Substrate node which contains `pallet-contracts` in the background
-    #[cfg(all(test, feature = "e2e-tests"))]
-    mod e2e_tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
-        use super::*;
-
-        /// A helper function used for calling contract messages.
-        use ink_e2e::build_message;
-
-        /// The End-to-End test `Result` type.
-        type E2EResult<T> = std::result::Result<T, Box<dyn std::error::Error>>;
-
-        /// We test that we can upload and instantiate the contract using its default constructor.
-        #[ink_e2e::test]
-        async fn default_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
-            // Given
-            let constructor = GovernanceRef::default();
-
-            // When
-            let contract_account_id = client
-                .instantiate("governance", &ink_e2e::alice(), constructor, 0, None)
-                .await
-                .expect("instantiate failed")
-                .account_id;
-
-            // Then
-            let get = build_message::<GovernanceRef>(contract_account_id.clone())
-                .call(|governance| governance.get());
-            let get_result = client.call_dry_run(&ink_e2e::alice(), &get, 0, None).await;
-            assert!(matches!(get_result.return_value(), false));
-
-            Ok(())
-        }
-
-        /// We test that we can read and write a value from the on-chain contract contract.
-        #[ink_e2e::test]
-        async fn it_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
-            // Given
-            let constructor = GovernanceRef::new(false);
-            let contract_account_id = client
-                .instantiate("governance", &ink_e2e::bob(), constructor, 0, None)
-                .await
-                .expect("instantiate failed")
-                .account_id;
-
-            let get = build_message::<GovernanceRef>(contract_account_id.clone())
-                .call(|governance| governance.get());
-            let get_result = client.call_dry_run(&ink_e2e::bob(), &get, 0, None).await;
-            assert!(matches!(get_result.return_value(), false));
-
-            // When
-            let flip = build_message::<GovernanceRef>(contract_account_id.clone())
-                .call(|governance| governance.flip());
-            let _flip_result = client
-                .call(&ink_e2e::bob(), flip, 0, None)
-                .await
-                .expect("flip failed");
-
-            // Then
-            let get = build_message::<GovernanceRef>(contract_account_id.clone())
-                .call(|governance| governance.get());
-            let get_result = client.call_dry_run(&ink_e2e::bob(), &get, 0, None).await;
-            assert!(matches!(get_result.return_value(), true));
-
-            Ok(())
-        }
-    }
 }
